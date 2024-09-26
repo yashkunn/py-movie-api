a9efdb0f8744ffa0b8a37cfb8a570a42ce3a7aac
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-26 21:21:00 +0300

    third try

--- cinema/views.py
+++ cinema/views.py
@@ -14,12 +14,12 @@ def movie_list(request):
         serializer = MovieSerializer(movies, many=True)
         print(serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -34,6 +34,6 @@ def movie_detail(request, pk):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    movie.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
